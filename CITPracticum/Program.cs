e5978e20f27ed403c36aa3cc3c43c2eec807812b
unknown <wanatdaphengp@gmail.com>
2024-03-09 21:04:21 -0700

    Finalized login page styling

--- CITPracticum/Program.cs
+++ CITPracticum/Program.cs
@@ -54,7 +54,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 app.MapRazorPages();
 
 app.Run();
