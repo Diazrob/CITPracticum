b788b5ffe0dac12c129b31db8f4df96fe7b1edda
unknown <wanatdaphengp@gmail.com>
2024-03-10 23:08:48 -0600

    Created resume and cover letter pages, and added in new nav links

---
+++ CITPracticum/Controllers/CoverletterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CITPracticum.Controllers
+{
+    public class CoverletterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CITPracticum/Controllers/ResumeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CITPracticum.Controllers
+{
+    public class ResumeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
