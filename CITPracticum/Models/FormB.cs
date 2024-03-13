9b6f77f5041ee51365c2ade0aa761b397261ca5d
diazrob <diazrobee@gmail.com>
2024-03-12 21:27:15 -0600

    added models FormA,FormB, Address, PracticumForms

---
+++ CITPracticum/Models/FormB.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CITPracticum.Models
+{
+    public class FormB
+    {
+        public int Id { get; set; }
+        public string PracHost { get; set; }
+        public string StuName { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public string OrgName { get; set; }
+        public string PracSV{ get; set; }
+        [ForeignKey("Address")]
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+        public string Position { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+
+    }
+}
---
+++ CITPracticum/Models/PracticumForms.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CITPracticum.Models
+{
+    public class PracticumForms
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("FormA")]
+        public int FormAId { get; set; }
+        public FormA FormA { get; set; }
+
+    }
+}
