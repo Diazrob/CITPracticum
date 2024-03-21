b181c21fea7ef9311d787c2aa5a64ac8c9fe6029
diazrob <diazrobee@gmail.com>
2024-03-21 00:21:23 -0600

    updated nav based on User Roles

---
+++ CITPracticum/Data/Migrations/20240319170023_Updating-Student-model.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CITPracticum.Data.Migrations
+{
+    public partial class UpdatingStudentmodel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Document_DocumentId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DocumentId",
+                table: "Students",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Document_DocumentId",
+                table: "Students",
+                column: "DocumentId",
+                principalTable: "Document",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Document_DocumentId",
+                table: "Students");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "DocumentId",
+                table: "Students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Document_DocumentId",
+                table: "Students",
+                column: "DocumentId",
+                principalTable: "Document",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
